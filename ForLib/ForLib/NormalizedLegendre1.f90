a5e7a2efee8a0277cf52a256b3c329d848122917
Yu Zhang <whuzhangyu@gmail.com>
2016-05-26 22:50:19 -0400

    finish normalizedlegendre1

--- ForLib/ForLib/NormalizedLegendre1.f90
+++ ForLib/ForLib/NormalizedLegendre1.f90
@@ -1,39 +1,54 @@
-subroutine NormalizedLegendre1(n, m, t, p)
+module Legendre
+    contains
+    
+subroutine NormalizedLegendre1(n, t, p)
 !****************************************************************************
 ! Function  : compute the fully nromalized associated legendre function of 
-!             the 1st kind
+!             the 1st kind Pnm(t), up to degree n and order n
 ! Input     :
-!             n, m      - are degree and order, start from 0
+!             n, n      - degree and order, start from 0
 !             t         - parameter of Legendre function
 ! Output    : 
-!             p         - Legendre polynomials, (n+1)*(m+1)
+!             p         - Legendre polynomials, (n+1)*(n+1)
 ! Author    : Yu Zhang, zhang.6345@osu.edu, Ohio State University
 !****************************************************************************
 
     implicit none
     ! Variables
-    integer, intent(in) :: n, m
+    integer, intent(in) :: n
     real, intent(in)    :: t
     real                :: p(:,:)
-    integer             :: pr, pc   ! size of array p
-    real                :: a, b
+    integer             :: pr, pc   ! index of array p
+    integer             :: i, j
+    real                :: a, b, s
     
-    pr = n + 1
-    pc = m + 1
-    ! stored by column
-    if (n==0 .and. m==0) then
-        p(pc, pr)=1.d0
-    elseif (n==1 .and. m==1) then
-        p(pc, pr)=sqrt(3.d0*(1.d0-t**2.d0))
-    elseif (n>=1 .and. m==n-1) then
-        p(pc, pr)=sqrt(2.d0*n+1.d0)*t*p(pr-1,pr-1)
-    elseif (n>=2 .and. n==m) then
-        p(pc, pr)=sqrt((2.d0*n+1)/(2.d0*n)*(1.d0-t**2))*p(pr-1,pr-1)
-    elseif (n>=2 .and. m>=0 .and. m<=(n-2)) then
-        a=sqrt((2.d0*n-1.d0)*(2.d0*n+1.d0)/(n-m)/(n+m))
-        b=sqrt((2.d0*n+1.d0)*(n+m-1.d0)*(n-m-1.d0)/(2.d0*n-3.d0)/(n+m)/(n-m))
-        p(pc, pr)=a*t*p(pc,pr-1) - b*p(pc,pr-2)
-    endif
+    
+    do i = 0, n
+        s = 0.d0
+        do j = 0, i
+            pr = i + 1
+            pc = j + 1
+            ! row major
+            if (i==0 .and. j==0) then
+                p(pr,pc)=1.d0
+            elseif (i==1 .and. j==1) then
+                p(pr,pc)=sqrt(3.d0*(1.d0-t**2.d0))
+            elseif (i>=1 .and. j==i-1) then
+                p(pr,pc)=sqrt(2.d0*i+1.d0)*t*p(pr-1,pr-1)
+            elseif (i>=2 .and. i==j) then
+                p(pr,pc)=sqrt((2.d0*i+1)/(2.d0*i)*(1.d0-t**2))*p(pr-1,pr-1)
+            elseif (i>=2 .and. j>=0 .and. j<=(i-2)) then
+                a=sqrt((2.d0*i-1.d0)*(2.d0*i+1.d0)/(i-j)/(i+j))
+                b=sqrt((2.d0*i+1.d0)*(i+j-1.d0)*(i-j-1.d0)/(2.d0*i-3.d0)/(i+j)/(i-j))
+                p(pr,pc)=a*t*p(pr-1,pc) - b*p(pr-2,pc)
+            endif
+            ! compute the error
+            !s=s+p(pr,pc)**2
+        enddo
+        !print *, i, s-2.d0*i-1.d0
+    enddo
     
     return
 end subroutine
+
+end module
--- ForLib/ForLib/xNormalizedLegendre.f90
+++ ForLib/ForLib/xNormalizedLegendre.f90
@@ -1,13 +1,20 @@
 program xNormalizedLegendre
 !****************************************************************************
-! Function  : test the subroutine NormalizedLegendre
+! Function  : test the subroutine NormalizedLegendre1
 ! Author    : Yu Zhang, zhang.6345@osu.edu     
 !****************************************************************************
+    use Legendre
     implicit none
     ! Variables
-     
-    real    :: NormalizedLegendre
+    integer, parameter :: n=5
+    real    :: pi, t, p(n,n)
+    integer :: i
     
+    pi = 4.d0 * atan(1.d0)
+    t = cos(39.d0/180.d0*pi)
+    call NormalizedLegendre1(n-1,t,p)
     
-    
+    do i=1, n
+        print *, p(i,:)
+    enddo
 end program
